4725526de3abd6b6df8d9f1fdb09fe980f90138b
smirnovich <daniil_smirnov@yahoo.com>
2025-09-18 14:53:44 +0200

    optimized with grep

--- finder-app/finder.sh
+++ finder-app/finder.sh
@@ -2,7 +2,7 @@
 num=$#
 if [ $num -lt 2 ];
 then
-	return 1
+	exit 1
 fi
  
 filesdir=$1
@@ -10,10 +10,9 @@ searchstr=$2
 
 if test $filesdir 
 then
-	nolines=$(ls $filesdir | grep $searchstr | wc -l)
-	echo $nolines
+	nolines=$(grep -rnw $filesdir -e $searchstr | wc -l)
 	nofiles=$(ls $filesdir | wc -l)
 	echo "The number of files are $nofiles and the number of matching lines are $nolines"
 else
-	return 1
+	exit 1
 fi
