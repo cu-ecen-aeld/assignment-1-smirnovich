5a5d2aea7f81c0019005d12f747235400eb3a64d
smirnovich <daniil_smirnov@yahoo.com>
2025-09-19 16:12:43 +0200

    cleaning modifying

--- finder-app/writer.c
+++ finder-app/writer.c
@@ -1,13 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <errno.h>
 int main(int argc, char* argv[]){
     if (argc < 3){
-        fprintf(stderr, "values less, %d\r\n", argc);
-        return EXIT_FAILURE;
+   //     fprintf(stderr, "values less, %d\r\n", argc);
+        exit(1);// EXIT_FAILURE;
     }
     FILE *fptr;
 
     fptr = fopen(argv[1], "w");
+ /*   if (fptr == NULL) {
+        printf("%d\n", errno);
+    }*/
     fputs(argv[2],fptr);
     fclose(fptr);
     return EXIT_SUCCESS;
